b7982be59c3e9daa8a8cb23943a02b61c78743f6
Thomas Swann <tkswann@mac.com>
2017-05-03 17:41:31 -0500

    letterBoxes loading dynamically, added basic styles for them

--- LED_Text_Parser/index.js
+++ LED_Text_Parser/index.js
@@ -1,4 +1,48 @@
-const h1 = document.getElementById(`title`)
-h1.className = `huge`
+// grab all necessary DOM elements for manipulation
+const container = document.getElementById('container')
+  ,     display = document.getElementById('display')
+  ,   textInput = document.getElementById('input')
+  ,  textOutput = document.getElementById('output')
+  ,      matrix = []
+  ,        word = 'hello'
 
-console.log(`hello`)
+const switchOnOrOff = (led) => {
+  if(led.className === 'led off') {
+    led.className = 'led'
+  }
+  else {
+    led.className = 'led off'
+  }
+}
+
+// const newLine = () => {
+//   let line = []
+//   for (let i = 0; i < 25; i++) {
+//     let led = document.createElement('div')
+//     led.onclick = function () {switchOnOrOff(this)}
+//     led.className = 'led off'
+//     display.appendChild(led)
+//     line[i] = led
+//   }
+//   return line
+// }
+
+// for (let i = 0; i < 7; i++)
+//   matrix[i] = newLine()
+
+const newLetterBox = () => {
+  [...word].forEach((letter, index) => {
+     let letterBox = document.createElement('div')
+     letterBox.setAttribute('class', 'letterBox')
+     letterBox.setAttribute('id', `letterBox${index}`)
+     display.appendChild(letterBox)
+
+     for (let i = 0; i < 35; i++) {
+       let led = document.createElement('div')
+       led.onclick = function () {switchOnOrOff(this)}
+       led.className = 'led off'
+       document.getElementById(`letterBox${index}`).appendChild(led)
+     }
+  })
+}
+newLetterBox()
