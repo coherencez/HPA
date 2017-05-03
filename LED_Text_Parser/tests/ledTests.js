7dd8ce854a214ecf72f12673e2e6796b8be3e5cc
Thomas Swann <tkswann@mac.com>
2017-05-03 15:44:17 -0500

    added Tape for unit testing

---
+++ LED_Text_Parser/tests/ledTests.js
@@ -0,0 +1,9 @@
+const test = require('tape')
+
+test('LED Screen Helper Functions', (t) => {
+  const actual = 0
+    , expected = 0
+
+  t.equal(actual, expected, 'actual should equal expected')
+  t.end()
+})
