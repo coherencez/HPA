adf43cbbe4acbb516a0b7c1366c9b52480615434
Thomas Swann <tkswann@mac.com>
2017-05-04 02:31:56 -0500

    completed getCoordinate function to build alphanumeric coordinates dictionary in JSON

--- LED_Text_Parser/index.js
+++ LED_Text_Parser/index.js
@@ -4,10 +4,13 @@ const container = document.getElementById('container')
   ,     display = document.getElementById('display')
   ,   textInput = document.getElementById('input')
   ,  textOutput = document.getElementById('output')
+  ,      button = document.getElementById('button')
   ,      matrix = []
-  ,    testWord = 'hello'
+  ,    testWord = 'h'
 
-// changes LED's `on` status by changing the class name
+/******************** HELPER FUNCTIONS **************************/
+
+// switch "LED" div's className on or off
 const switchOnOrOff = (led) => {
   if(led.className === 'led off') {
     led.className = 'led'
@@ -26,22 +29,45 @@ const createLetterBox = (id, classAssignment = 'letterBox') => {
 }
 
 const populateLetterBoxWithLEDs = (index) => {
-  let line = []
+  let lines = []
   for (let i = 0; i < 7; i++) {
-    let divs = []
+    let ledDivs = []
     for (let j = 0; j < 5; j++) {
       let led = document.createElement('div')
       // pre es6 function used for contextual `this`
       led.onclick = function () { switchOnOrOff(this) }
       led.className = 'led off'
       document.getElementById(`letterBox${index}`).appendChild(led)
-      divs.push(led)
+      ledDivs.push(led)
     }
-    line.push(divs)
+    lines.push(ledDivs)
   }
-  matrix.push(line)
+  matrix.push(lines)
 }
 
+const getCoords = () =>  {
+  // using destructuring to clone the array for
+  // safe manipulation and more declarative syntax
+  const [...letterBoxes] = matrix
+    ,   coords = []
+
+    letterBoxes.forEach((row, i) => {
+      row.forEach((divs, j) => {
+        divs.forEach((led, n) => {
+          // push a `coordinates` object into our coords array
+          // containg the 3 index values for each LED needed to
+          // write the alphanumeric characters
+          if(led.className === 'led') {
+            coords.push({i,j,n})
+          }
+        })
+      })
+    })
+  console.log(JSON.stringify(coords))
+}
+
+button.addEventListener('click', getCoords)
+/*****************************************************************/
 const H = [0,0,0,4,1,0,1,4,2,0,2,4,3,0,3,1,3,2,3,3,3,4,4,0,4,4,5,0,5,4,6,0,6,4]
 
 const newLetterBoxes = (word) => {
@@ -64,7 +90,7 @@ x = letterBox, y = row, z = LED in row
 // const write = (arr) => {
 //   var i = 0;
 //   while (i < arr.length){
-//     matrix[arr[i++]][arr[i++]].className = "led";
+//     matrix[arr[i++]][arr[i++]][arr[i++]].className = "led";
 //   }
 // }
 // write(H)
