c94a7bb1870a2c7937fd436e8dd8e44706e69cb9
Thomas Swann <tkswann@mac.com>
2017-05-06 02:00:42 -0500

    added pug, cheerio, body-parser; setup server to render views, and take input from webpage

--- Web_Crawler/server.js
+++ Web_Crawler/server.js
@@ -1,4 +1,27 @@
-const app = require('express')()
+'use strict'
+// THIRD PARTY MODULES
+const express = require('express')
+  ,       app = express()
+  ,bodyParser = require('body-parser')
+
+// APP CONSTANTS
 const PORT = 3000
 
-app.listen(PORT)
+// SETTINGS
+app.set('port', PORT)
+app.set('view engine', 'pug')
+
+// MIDDLEWARE
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log(`this is the form request`, req.body)
+  res.redirect('/')
+})
+app.listen(PORT, () => console.log(`Now listening on port ${PORT}`))
