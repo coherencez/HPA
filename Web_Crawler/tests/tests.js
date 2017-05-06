0460df80391bde63834bfbd22e0a23e7a531f25e
Thomas Swann <tkswann@mac.com>
2017-05-06 01:14:01 -0500

    boilerplate files/structure, tape for testing, express for server

---
+++ Web_Crawler/tests/tests.js
@@ -0,0 +1,10 @@
+const test = require('tape')
+
+test('LED Screen Helper Functions', (t) => {
+  const    msg = 'actual should equal expected'
+    ,   actual = 0
+    , expected = 0
+
+  t.equal(actual, expected, msg)
+  t.end()
+})
